471361cd7b2e2720cd33dfa03544dd050304fd1f
Mehul Ved <mehul.ved@nexsales.com>
2021-06-22 02:09:07 +0530

    Make DNS changes

---
+++ infra/dns.tf
@@ -0,0 +1,15 @@
+resource "aws_route53_zone" "primary" {
+  name = "lvh.me"
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "lvh.me"
+  type    = "A"
+
+  alias {
+    name                   = module.alb.lb_dns_name
+    zone_id                = module.alb.lb_zone_id
+    evaluate_target_health = true
+  }
+}
